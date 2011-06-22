c6f1910248ea948841cdb435a4674bb70dc7749c
Stephan Toggweiler <stephan.toggweiler@gmail.com>
2011-06-23 00:10:14 +0200

    base

--- presenter.rb
+++ presenter.rb
@@ -1,7 +1,10 @@
+#!/usr/bin/env ruby
 #=presenter.rb
 
 require 'sinatra'
 require 'haml'
+
+PRESENTER_BASE=File.dirname(__FILE__)
 
 configure(:development) do |c|
   require "sinatra/reloader"
@@ -9,7 +12,71 @@ configure(:development) do |c|
 end
 
 get '/' do
+  @pres=[]
+  Dir.open("#{PRESENTER_BASE}/data").each do |d|
+    next if d=~/^\./
+    @pres<<d
+  end
   haml :index
+end
+
+get '/view/:present' do
+  unless File.exist?("#{PRESENTER_BASE}/data/#{params["present"]}")
+    return [302, {"Location"=>"/"}, ["Redirect"]]
+  end
+  @img="/none"
+  if File.exist?("#{PRESENTER_BASE}/data/#{params["present"]}/active")
+    f=File.read("#{PRESENTER_BASE}/data/#{params["present"]}/active")
+    @img="/images/#{params["present"]}/#{f}"
+  end
+  haml :view
+end
+
+get '/active/:present' do
+  unless File.exist?("#{PRESENTER_BASE}/data/#{params["present"]}/active")
+    return [200, {}, ["/none"]]
+  end
+  f=File.read("#{PRESENTER_BASE}/data/#{params["present"]}/active")
+  return [200, {}, ["/images/#{params["present"]}/#{f}"]]
+end
+
+get '/images/:present/:img' do
+  unless File.exist?("#{PRESENTER_BASE}/data/#{params["present"]}/img/#{params["img"]}")
+    return [302, {"Location"=>"/"}, ["Redirect"]]
+  end
+  send_file "#{PRESENTER_BASE}/data/#{params["present"]}/img/#{params["img"]}"
+end
+
+post '/admin/:present/:img' do
+  unless File.exist?("#{PRESENTER_BASE}/data/#{params["present"]}/img/#{params["img"]}")
+    return [200, {}, ["No such image"]]
+  end
+  File.open("#{PRESENTER_BASE}/data/#{params["present"]}/active", "w") do |f|
+    f.puts(params["img"])
+  end
+  [200, {}, ["Activated"]]
+end
+
+get '/admin/:present' do
+  unless File.exist?("#{PRESENTER_BASE}/data/#{params["present"]}")
+    return [302, {"Location"=>"/admin"}, ["Redirect"]]
+  end
+  @pres=[]
+  Dir.open("#{PRESENTER_BASE}/data/#{params["present"]}/img").each do |d|
+    next if d=~/^\./
+    @pres<<d
+  end
+  @pres.sort!
+  haml :admin_present
+end
+
+get '/admin' do
+  @pres=[]
+  Dir.open("#{PRESENTER_BASE}/data").each do |d|
+    next if d=~/^\./
+    @pres<<d
+  end
+  haml :admin
 end
 
 #=EOF
--- public/javascript/ajax.js
+++ public/javascript/ajax.js
@@ -1,22 +1,33 @@
-<script>
 function createXMLHttpRequest() {
-try { return new XMLHttpRequest(); } catch(e) {}
-try { return new ActiveXObject("Msxml2.XMLHTTP"); } catch (e) {}
-return null;
+  try { return new XMLHttpRequest(); } catch(e) {}
+  try { return new ActiveXObject("Msxml2.XMLHTTP"); } catch (e) {}
+  return null;
 }
-function mobileToken() {
-document.getElementById('mobile_token').innerHTML='Mobile Token wird angefordert...';
-xmlhttp=createXMLHttpRequest();
-xmlhttp.onreadystatechange=function() {
-if (xmlhttp.readyState==4 && xmlhttp.status==200) {
-document.getElementById('mobile_token').innerHTML=xmlhttp.responseText;
+
+function imageChange(_present) {
+  document.getElementById('state').innerHTML='search new image...';
+  xmlhttp=createXMLHttpRequest();
+  xmlhttp.onreadystatechange=function() {
+    if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+      document.getElementById('present').src=xmlhttp.responseText;
+    }
+  }
+  xmlhttp.open("GET","/active/"+_present,true);
+  xmlhttp.setRequestHeader('Content-Type','application/x-www-form-urlencoded');
+  xmlhttp.send("AKTIVATE");
+  setTimeout(imageChange(_present), 10);
+  document.getElementById('state').innerHTML='waitung...';
 }
+
+function activatePresent(_present) {
+  document.getElementById('present').innerHTML=_present+' wird aktiviert...';
+  xmlhttp=createXMLHttpRequest();
+  xmlhttp.onreadystatechange=function() {
+    if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+      document.getElementById('present').innerHTML=xmlhttp.responseText;
+    }
+  }
+  xmlhttp.open("POST",_present,true);
+  xmlhttp.setRequestHeader('Content-Type','application/x-www-form-urlencoded');
+  xmlhttp.send("ACTIVATE");
 }
-user=document.forms[0].USER_ID.value;
-pwd=encodeURIComponent(document.forms[0].PASSWORD.value);
-xmlhttp.open("POST","request_mobile_token.html",true);
-xmlhttp.setRequestHeader('Content-Type','application/x-www-form-urlencoded');
-xmlhttp.send("USER_ID="+user+"&PASSWORD="+pwd);
-document.forms[0].TOKEN.focus();
-}
-</script>
