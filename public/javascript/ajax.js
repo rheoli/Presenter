1d697aaea19d3819353472e9f2486375cd81e3a8
Stephan Toggweiler <stephan.toggweiler@gmail.com>
2011-06-26 16:51:10 +0200

    Fixes

--- public/javascript/ajax.js
+++ public/javascript/ajax.js
@@ -5,17 +5,24 @@ function createXMLHttpRequest() {
 }
 
 function imageChange(_present) {
+  //alert("Hallo");
   document.getElementById('state').innerHTML='search new image...';
   xmlhttp=createXMLHttpRequest();
   xmlhttp.onreadystatechange=function() {
+	document.getElementById('state').innerHTML="State "+xmlhttp.readyState+" / status "+xmlhttp.status+" / "+xmlhttp.responseText;
     if (xmlhttp.readyState==4 && xmlhttp.status==200) {
-      document.getElementById('present').src=xmlhttp.responseText;
+      document.getElementById('present').innerHTML="<img src='"+xmlhttp.responseText+"' />";
+      document.getElementById('state').innerHTML='Image Set';
+      setTimeout("imageChange('"+_present+"')", 2000);
     }
+    else if (xmlhttp.readyState==4 && xmlhttp.status==0) {
+	  document.getElementById('state').innerHTML='Error to load';
+	  setTimeout("imageChange('"+_present+"')", 5000);
+	}
   }
   xmlhttp.open("GET","/active/"+_present,true);
   xmlhttp.setRequestHeader('Content-Type','application/x-www-form-urlencoded');
-  xmlhttp.send("AKTIVATE");
-  setTimeout(imageChange(_present), 10);
+  xmlhttp.send("ACTIVATE");
   document.getElementById('state').innerHTML='waitung...';
 }
 
